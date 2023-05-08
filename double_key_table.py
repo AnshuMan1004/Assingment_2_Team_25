c8806692cac2a4c53c27df4e9a54e498f2048719
Sean Lee <slee0206@student.monash.edu>
2023-05-09 02:06:24 +1000

    updates

--- double_key_table.py
+++ double_key_table.py
@@ -31,15 +31,19 @@ class DoubleKeyTable(Generic[K1, K2, V]):
 
         #create the underlying array. If sizes is not None, the provided array should replace the existing TABLE_SIZES to decide the size of the top-level hash table. If internal_sizes is not None, the provided array should replace the existing TABLE_SIZES for the internal hash tables (See hash_table.py for an example)).
         
-        #self.sizes.index
-
-        self.table = ArrayR(self.TABLE_SIZES[0]) # should be 0 for the first one (self.size) = 0 
 
         if sizes is not None:
             sizes = self.TABLE_SIZES
 
         elif internal_sizes is not None:
             internal_sizes = self.TABLE_SIZES
+
+        table_sizes_index = 0
+
+        self.table = ArrayR(self.TABLE_SIZES[table_sizes_index])
+    
+         # should be 0 for the first one (self.size) = 0 
+
         
 
     def hash1(self, key: K1) -> int:
--- mountain_organiser.py
+++ mountain_organiser.py
@@ -7,8 +7,10 @@ class MountainOrganiser:
     def __init__(self) -> None:
         raise NotImplementedError()
 
-    def cur_position(self, mountain: Mountain) -> int:
+    def cur_position(self, mountain: Mountain) -> int: 
+        #Finds the rank of the provided mountain given all mountains included so far.
+        #Raises KeyError if this mountain hasn't been added yet.
         raise NotImplementedError()
 
-    def add_mountains(self, mountains: list[Mountain]) -> None:
+    def add_mountains(self, mountains: list[Mountain]) -> None: # adds a list of mountains to the organiser
         raise NotImplementedError()
