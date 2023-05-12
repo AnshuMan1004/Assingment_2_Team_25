086348042173c8389a5265bb46b341f894e90a77
AnshuMan1004 <128580088+AnshuMan1004@users.noreply.github.com>
2023-05-12 20:03:49 +1000

    Finishing mountain_manager

--- mountain_manager.py
+++ mountain_manager.py
@@ -3,19 +3,29 @@ from mountain import Mountain
 class MountainManager:
 
     def __init__(self) -> None:
-        pass
+        self.mountains = []
 
     def add_mountain(self, mountain: Mountain):
-        raise NotImplementedError()
+        self.mountains.append(mountain)
 
     def remove_mountain(self, mountain: Mountain):
-        raise NotImplementedError()
+        self.mountains.remove(mountain)
 
     def edit_mountain(self, old: Mountain, new: Mountain):
-        raise NotImplementedError()
+        index = self.mountains.index(old_mountain)
+        self.mountains[index] = new_mountain
 
     def mountains_with_difficulty(self, diff: int):
-        raise NotImplementedError()
+        return [m for m in self.mountains if m.difficulty_level == diff]
 
     def group_by_difficulty(self):
-        raise NotImplementedError()
+        sorted_mountains = sorted(self.mountains, key=lambda m: m.difficulty_level)
+        grouped_mountains = []
+
+        for m in sorted_mountains:
+            if not grouped_mountains or grouped_mountains[-1][0].difficulty_level != m.difficulty_level:
+                grouped_mountains.append([m])
+            else:
+                grouped_mountains[-1].append(m)
+
+        return grouped_mountains
